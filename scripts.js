aa73f72cc1f68e95dddfdbf49b3087fadfccd8c8
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-25 21:47:12 -0500

    feat: :sparkles: Start on JavaScirpt and begin testing submitting max and min
    Create the 'const's, create 'p' tag, start on implementing max and min submitions
    
    n/a

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
+const maxnum = document.querySelector('#max-form');
+const minnum = document.querySelector('#min-form');
+const color = document.querySelector('#color');
+const p = document.createElement('p');
+document.body.appendChild(p);
 
+maxnum.addEventListener('submit', event => {
+  event.preventDefault();
+  const newmax = event.target.elements[0].value;
+  console.log(newmax);
+});
+
+minnum.addEventListener('submit', event => {
+  event.preventDefault();
+  const newmin = event.target.elements[0].value;
+  console.log(newmin);
+});
