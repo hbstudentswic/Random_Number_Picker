4f28775ad28e8f80d3a2bde506ac9571d982425a
Hunter Bossetto <hbstudentswic@gmail.com>
2021-04-25 22:22:05 -0500

    feat: :zap: Create event handler for changing color and work on changing min and max for display
    Create the color changing event handler and start working on the event that will display the random number in the 'p' tag
    
    n/a

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const maxnum = document.querySelector('#max-form');
 const minnum = document.querySelector('#min-form');
 const color = document.querySelector('#color');
+const root = document.querySelector(':root');
+const gennum = document.querySelector('#generate-num-form');
 const p = document.createElement('p');
 document.body.appendChild(p);
 
@@ -14,4 +16,16 @@ minnum.addEventListener('submit', event => {
   event.preventDefault();
   const newmin = event.target.elements[0].value;
   console.log(newmin);
+});
+
+color.addEventListener('input', event => {
+  root.style.setProperty('--color', event.target.value);
+});
+
+gennum.addEventListener('submit', event => {
+  event.preventDefault();
+
+  function getRandomArbitrary(newmin, newmax) {
+    return Math.random() * (newmin - newmax) + newmax;
+  }
 });
